08bfac1114cb889c0b875df22f4873a8bfcbb880
DystDev <58528580+DystDev@users.noreply.github.com>
2021-08-03 18:01:06 +0100

    Color Change (hook)

--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -1,15 +1,21 @@
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NetworkedPlayer : NetworkBehaviour
 {
+
+    [SerializeField] private TMP_Text dispNameText = null;
+    [SerializeField] private Renderer dispColorRenderer = null;
+
+
     [SyncVar]
     [SerializeField]
     private string dispName = "Missing";
 
-    [SyncVar]
+    [SyncVar(hook = nameof(HandlePlayerColorChange))]
     [SerializeField]
     private Color dispColor = Color.black;
 
@@ -24,5 +30,10 @@ public class NetworkedPlayer : NetworkBehaviour
     public void SetDispColor(Color newDispColor)
     {
         dispColor = newDispColor;
+    }
+    
+    private void HandlePlayerColorChange(Color oldColor, Color newColor)
+    {
+        dispColorRenderer.material.SetColor("_BaseColor", newColor);
     }
 }
