59abe83e050f95b9a70bed550f7fcd0a88e7409f
DystDev <58528580+DystDev@users.noreply.github.com>
2021-08-05 13:01:39 +0100

    Suitable Username Checker (Server Authority)
    - Added ProfanityWords.cs and AllowedLetters.cs containing info for username checking
    - Server Authority

--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -11,7 +11,7 @@ public class NetworkedPlayer : NetworkBehaviour
     [SerializeField] private Renderer dispColorRenderer = null;
 
 
-    [SyncVar]
+    [SyncVar(hook = nameof(HandlePlayerNameChange))]
     [SerializeField]
     private string dispName = "Missing";
 
@@ -19,6 +19,7 @@ public class NetworkedPlayer : NetworkBehaviour
     [SerializeField]
     private Color dispColor = Color.black;
 
+    #region Server
 
     [Server]
     public void SetDispName(string newDispName)
@@ -31,9 +32,77 @@ public class NetworkedPlayer : NetworkBehaviour
     {
         dispColor = newDispColor;
     }
-    
+
+    [Server]
+    public bool IsSuitable(string word)
+    {
+        
+        foreach (string badWord in ProfanityWords.profanityArray)
+        {
+            if (word.Contains(badWord) == true)
+            {
+                return false;
+            }
+        }
+        foreach (char letter in word)
+        {
+            foreach (char letterInAllowed in AllowedLetters.allowedChars)
+            {
+                if ((letter == letterInAllowed) == false)
+                {
+                    return false;
+                }
+            }
+        }
+        if (word.Contains(" ") == true)
+        {
+            return false;
+        }
+        else if (word.Length < 3 || word.Length > 10)
+        {
+            return false;
+        }
+        else { return true; }
+    }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        
+        if (IsSuitable(newDisplayName) == false)
+        {
+            return;
+        }
+        SetDispName(newDisplayName);
+        RpcNotifyDisplayName(newDisplayName);
+
+    }
+
+    [ClientRpc]
+    private void RpcNotifyDisplayName(string newName)
+    {
+        Debug.Log($"New name: {newName}");
+    }
+
+    #endregion
+
+    #region Client
+
+
     private void HandlePlayerColorChange(Color oldColor, Color newColor)
     {
         dispColorRenderer.material.SetColor("_BaseColor", newColor);
     }
+
+    private void HandlePlayerNameChange(string oldName, string newName) 
+    {
+        dispNameText.text = newName;
+    }
+    [ContextMenu("Set Name")]
+    public void ClientSetName()
+    {
+        CmdSetDisplayName("Name has been set from here"); 
+    }
+
+    #endregion
 }
